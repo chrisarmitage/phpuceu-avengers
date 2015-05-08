0313ef3ee6c42b91f018435a1d5743f207e63271
Chris Armitage <dev@chrisarmitage.com>
2015-05-08 17:12:05 +0200

    AVENGERS ASSEMBLE

--- src/Avengers/AvengersInitiative.php
+++ src/Avengers/AvengersInitiative.php
@@ -5,6 +5,7 @@ namespace Avengers;
 class AvengersInitiative
 {
     protected $availableAvengers;
+    protected $assembledAvengers;
     
     public function addAvenger(\Avengers\Avenger $avenger) {
         $this->availableAvengers[] = $avenger;
@@ -12,5 +13,13 @@ class AvengersInitiative
     
     public function getAvailableAvengers() {
         return $this->availableAvengers;
+    }
+    
+    public function getAssembledAvengers() {
+        return $this->assembledAvengers;
+    }
+    
+    public function assemble() {
+        $this->assembledAvengers = $this->availableAvengers;
     }
 }
--- tests/Avengers/AvengersInitiativeTest.php
+++ tests/Avengers/AvengersInitiativeTest.php
@@ -29,4 +29,20 @@ class AvengersInitiativeTest extends \PHPUnit_Framework_TestCase
         $availableAvengers = $initiative->getAvailableAvengers();        
         $this->assertEquals(1, count($availableAvengers));
     }
+    
+    public function testAvengersCanBeAssembled() {
+        $initiative = new \Avengers\AvengersInitiative();
+
+        $avenger = M::mock('Avengers\Avenger');
+        $initiative->addAvenger($avenger);
+        
+        $assembledAvengers = $initiative->getAssembledAvengers();        
+        $this->assertEquals(0, count($assembledAvengers));
+        
+        $initiative->assemble();
+        
+        $assembledAvengers = $initiative->getAssembledAvengers();        
+        $this->assertEquals(1, count($assembledAvengers));
+        
+    }
 }
