86aa60c2a66bf2ac053ea59335a325e4fa81d4a2
JamesMallison <james.mallison@cybertill.co.uk>
2015-05-09 14:02:56 +0200

    Abstracted NickFury -> Director

--- src/Avengers/AvengersInitiative.php
+++ src/Avengers/AvengersInitiative.php
@@ -2,29 +2,32 @@
 
 namespace Avengers;
 
+use Avengers\People\Director,
+    Avengers\Avenger;
+
 class AvengersInitiative
 {
     protected $director;
-    
+
     protected $availableAvengers;
     protected $assembledAvengers;
-    
-    public function __construct(\Avengers\People\NickFury $director) {
+
+    public function __construct(Director $director) {
         $this->director = $director;
     }
-    
-    public function addAvenger(\Avengers\Avenger $avenger) {
+
+    public function addAvenger(Avenger $avenger) {
         $this->availableAvengers[] = $avenger;
     }
-    
+
     public function getAvailableAvengers() {
         return $this->availableAvengers;
     }
-    
+
     public function getAssembledAvengers() {
         return $this->assembledAvengers;
     }
-    
+
     public function assemble() {
         $this->assembledAvengers = $this->availableAvengers;
     }
